b07a7a78a219a3e5a9de231937e03b380925aa7e
Aakankshap16 <aakankshap971@gmail.com>
2023-04-20 19:36:12 +0530

    Greeting msg

---
+++ IndianStateCensusAnalyserProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStateCensusAnalyserProblem
+{
+    internal class Program
+    {
+       public  static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyser Problem!!! ");
+        }
+    }
+}
